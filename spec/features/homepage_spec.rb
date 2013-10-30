5fbca507dcd42059ee065e72b1e880d2e9c13755
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-30 16:45:36 +0200

    new page link

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 feature "Home Page" do
-  scenario "has default home page content" do
+
+  before do
     visit root_url
+  end
+
+  scenario "home page heading" do
     expect(page).to have_content("VSWiki Home Page")
+  end
+
+  scenario "a link to create a new page" do
+    expect(page).to have_link("Create a new page")
   end
 end
