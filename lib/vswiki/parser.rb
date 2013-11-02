e9e86d062e9bc6d7a42961669e3c53694e47d973
Ville Siltanen <ville.siltanen@iki.fi>
2013-11-02 17:55:56 +0200

    rudimentary external link support

--- lib/vswiki/parser.rb
+++ lib/vswiki/parser.rb
@@ -11,15 +11,26 @@ module Vswiki
     end
 
     def self.format_html(wikitext)
-      self.format_links(wikitext)
+      wikitext = self.format_wikilinks(wikitext)
+      wikitext = self.format_extlinks(wikitext)
     end
 
-    def self.format_links(wikitext)
+    def self.format_wikilinks(wikitext)
       links = wikitext.scan(/\[\[[^\]]*\]\]/)
       links.each do |link|
         linktext = link.gsub(/[\[\]]/, "")
         title = self.make_wikititle(linktext)
-        wikitext.gsub!(link, "<a href=\"/#{title}\">#{linktext}</a>")
+        wikitext.gsub!(link, "<a href=\"#{title}\">#{linktext}</a>")
+      end
+      wikitext
+    end
+
+    def self.format_extlinks(wikitext)
+      # not interested in verifying the correctness of the URL -
+      # that's the author's responsibility; just generate the anchor tag
+      extlinks = wikitext.scan(/https?:\/\/\S+/)
+      extlinks.each do |link|
+        wikitext.gsub!(link, "<a href=\"#{link}\">#{link}</a>")
       end
       wikitext
     end
--- spec/lib/vswiki/parser_spec.rb
+++ spec/lib/vswiki/parser_spec.rb
@@ -17,7 +17,19 @@ module Vswiki
       let(:link) { "[[some link]]" }
 
       it "creates an anchor tag for a wikilink" do
-        expect(Vswiki::Parser.format_html(link)).to eq '<a href="/SomeLink">some link</a>'
+        expect(Vswiki::Parser.format_html(link)).to eq '<a href="SomeLink">some link</a>'
+      end
+    end
+
+    describe "bare external links" do
+      let(:httplink) { "http://www.google.com" }
+      let(:httpslink) { "https://www.google.fi" }
+
+      it "creates an anchor tag for an external http(s) link" do
+        expect(Vswiki::Parser.format_html(httplink)).
+          to eq '<a href="http://www.google.com">http://www.google.com</a>'
+        expect(Vswiki::Parser.format_html(httpslink)).
+          to eq '<a href="https://www.google.fi">https://www.google.fi</a>'
       end
     end
   end
