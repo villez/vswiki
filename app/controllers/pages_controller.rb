91477528e64a79d89f55b277f8ee1b6d9082ce4a
Ville Siltanen <ville.siltanen@iki.fi>
2014-07-20 15:28:22 +0300

    refactoring and setting default time zone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
   before_filter :get_sidebar, only: [:show, :index]
 
   def index
-    if params[:go_to_page]  # the go-to-page miniform in the sidebar
-      redirect_to action: "show", id: Page.make_wikititle(params[:go_to_page])
+    if params[:go_to_page]
+      redirect_to action: "show", id: to_wikititle(params[:go_to_page])
     end
   end
 
@@ -14,10 +14,10 @@ class PagesController < ApplicationController
   end
 
   def create
-    @prev_page = Page.find_by(wikititle: Page.make_wikititle(params[:page][:title]))
+    @prev_page = Page.find_by(wikititle: to_wikititle(params[:page][:title]))
     @page = Page.create(page_params)
     if @page.valid?
-      redirect_to_show_or_edit
+      redirect_to_show_or_keep_editing
     else
       render :new
     end
@@ -27,8 +27,8 @@ class PagesController < ApplicationController
     return redirect_to new_page_path(title: params[:id]) unless @page
 
     if @page.redirect_to.present?
-      redirect_to page_path(Page.make_wikititle(@page.redirect_to)),
-      notice: redirect_message
+      redirect_to(page_path(to_wikititle(@page.redirect_to)),
+                  flash: {info: redirect_message})
     end
   end
 
@@ -37,7 +37,7 @@ class PagesController < ApplicationController
 
   def update
     if @page.update_attributes(page_params)
-      redirect_to_show_or_edit
+      redirect_to_show_or_keep_editing
     else
       # should not happen normally, as title not editable via form and
       # there are currently no validations on other attributes
@@ -66,12 +66,16 @@ class PagesController < ApplicationController
     params.require(:page).permit(:title, :wikitext, :redirect_to)
   end
 
-  def redirect_to_show_or_edit
+  def redirect_to_show_or_keep_editing
     if params[:save_and_edit]
       render "edit"
     else
       redirect_to @page
     end
+  end
+
+  def to_wikititle(text)
+    Page.make_wikititle(text)
   end
 
   def redirect_message
