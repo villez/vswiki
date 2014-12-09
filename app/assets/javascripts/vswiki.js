30b8b9959692097ea6308bd64d66f8120cd942ca
Ville Siltanen <ville.siltanen@iki.fi>
2014-12-09 14:59:24 +0200

    showing/hiding redirect on edit page based on whether it's empty

--- app/assets/javascripts/vswiki.js
+++ app/assets/javascripts/vswiki.js
@@ -1,17 +1,27 @@
 var ready = function() {
+
+    // if the "redirect to" field has contents, show it (hidden by default)
+    // also, hide the page contents field 
+    if ($('#redirect input').val()) {
+        $('#redirect').slideDown();
+        $('#hide-redirect').show();
+        $('#show-redirect').hide();
+        $('.wikitext-edit').slideUp();
+    }
+    
     $('#show-redirect').on('click', function(e) {
         e.preventDefault();
         $(this).hide();
         $('#hide-redirect').show();
-        $('.wikitext-edit').slideToggle();
-        $('#redirect').slideToggle();
+        $('#redirect').slideDown();
+        $('.wikitext-edit').slideUp();
     });
     $('#hide-redirect').on('click', function(e) {
         e.preventDefault();
         $(this).hide();
         $('#show-redirect').show();
-        $('#redirect').slideToggle();
-        $('.wikitext-edit').slideToggle();
+        $('#redirect').slideUp();
+        $('.wikitext-edit').slideDown();
     });
 };
 
--- spec/features/redirect_page_spec.rb
+++ spec/features/redirect_page_spec.rb
@@ -2,16 +2,35 @@ require "rails_helper"
 
 feature "Set a page to redirect to another page" do
 
+  let!(:nonredirect_page) { Page.create(title: "nonredirect", wikitext: "page without redirect") }
   let!(:target_page) { Page.create(title: "the target", wikitext: "target page contents") }
   let!(:redirector) { Page.create(title: "redirector", redirect_to: "the target") }
 
-  scenario "redirect field hidden on new page form by default", js: true do
+  scenario "redirect field is hidden on new page form, can be toggled", js: true do
     visit new_page_path
     expect(page).not_to have_selector("#redirect")
+    expect(page).to have_link("Add redirect")
     click_link "Add redirect"
     expect(page).to have_selector("#redirect")
     click_link "Hide redirect"
     expect(page).not_to have_selector("#redirect")    
+  end
+
+  scenario "redirect field is hidden on edit form if empty, can be toggled", js: true do
+    visit edit_page_path(nonredirect_page)
+    expect(page).to have_field("Page contents", text: nonredirect_page.wikitext)
+    expect(page).not_to have_selector("#redirect")
+    expect(page).to have_link("Add redirect")
+    click_link "Add redirect"
+    expect(page).to have_selector("#redirect")
+    click_link "Hide redirect"
+    expect(page).not_to have_selector("#redirect")    
+  end
+
+  scenario "redirect field is shown on edit if nonempty", js: true do
+    visit edit_page_path(redirector)
+    expect(page).to have_selector("#redirect")
+    expect(page).to have_link("Hide redirect")
   end
 
   scenario "create a new page with redirect" do
