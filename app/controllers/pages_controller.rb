51d7439f8483d5cdf7a49c8c78a00d22b28f920f
Ville Siltanen <ville.siltanen@iki.fi>
2014-12-20 12:19:34 +0200

    renaming before_filter to before_action in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  before_filter :get_page, only: [:show, :edit, :update, :destroy]
-  before_filter :get_sidebar, only: [:show, :index]
+  before_action :get_page, only: [:show, :edit, :update, :destroy]
+  before_action :get_sidebar, only: [:show, :index]
 
   def index
     if params[:go_to_page]
